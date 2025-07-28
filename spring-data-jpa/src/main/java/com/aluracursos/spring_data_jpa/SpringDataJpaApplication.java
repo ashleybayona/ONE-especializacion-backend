fff237c5102eea07000a197a766188b67ee92ecc
achli <ashley.bayonav@gmail.com>
2025-07-28 17:15:28 -0500

    base project

---
+++ spring-data-jpa/src/main/java/com/aluracursos/spring_data_jpa/SpringDataJpaApplication.java
@@ -0,0 +1,21 @@
+package com.aluracursos.spring_data_jpa;
+
+import com.aluracursos.spring_data_jpa.principal.Principal;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaApplication.class, args);
+	}
+
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.muestraElMenu();
+	}
+
+}
